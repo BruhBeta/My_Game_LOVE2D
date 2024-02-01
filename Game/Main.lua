30b229c5e7f2205d04f8c955dd4f8997a4a2d28b
Gabriel Ordozgoite <Gabrielordozgoite@gmail.com>
2024-02-01 01:06:14 -0400

    Commit

--- Game/Main.lua
+++ Game/Main.lua
@@ -1,108 +1,81 @@
 _G.love = require("love")
 
 function love.load()
+    background = love.graphics.newImage("Images/Background1.png")
     Sprite1 = love.graphics.newImage("Images/Sprite-0001.png")
     Sprite2 = love.graphics.newImage("Images/Sprite-0002.png")
-    background = love.graphics.newImage("Images/Background1.png")
     tree = love.graphics.newImage("Images/tree.png")
+    _G.var1 = true
     p1 = {
-        PosX = 20,
-        PosY = 350,
+        PosX = 200,
+        PosY = 200,
         Velocidade = 200,
         Direcao = "parado",
         largura = Sprite1:getWidth(),
-        altura = Sprite1:getHeight(),
+        altura = Sprite1:getHeight()
     }
-    arvores = {}  -- Tabela para armazenar informações sobre as árvores
 
-    for i = 1, 10 do
-        local novaArvore = {
-            _i = true,
-            largura = 80,
-            altura = 120,
-            PosX = math.random(0, love.graphics.getWidth() - 80),
-            PosY = math.random(0, love.graphics.getHeight() - 120)
-        }
-        table.insert(arvores, novaArvore)
-    end
+    arvores = {}
 end
 
 function love.update(dt)
-    DT = dt
 
-    local novoPosX = p1.PosX
-    local novoPosY = p1.PosY
+    -- Movimentação
 
     if love.keyboard.isDown("a") then
-        novoPosX = p1.PosX - p1.Velocidade * dt
-        p1.Direcao = "esquerda"
+        p1.PosX = p1.PosX - p1.Velocidade * dt
     end
     if love.keyboard.isDown("d") then
-        novoPosX = p1.PosX + p1.Velocidade * dt
-        p1.Direcao = "direita"
+        p1.PosX = p1.PosX + p1.Velocidade * dt
     end
     if love.keyboard.isDown("s") then
-        novoPosY = p1.PosY + p1.Velocidade * dt
-        p1.Direcao = "baixo"
+        p1.PosY = p1.PosY + p1.Velocidade * dt
     end
     if love.keyboard.isDown("w") then
-        novoPosY = p1.PosY - p1.Velocidade * dt
-        p1.Direcao = "cima"
+        p1.PosY = p1.PosY - p1.Velocidade * dt
     end
 
-    -- Impedir a passagem do background
+    -- Limitação de mapa
 
     if p1.PosX < 0 then
         p1.PosX = 0
     elseif p1.PosX > love.graphics.getWidth() - p1.largura then
         p1.PosX = love.graphics.getWidth() - p1.largura
     end
-
     if p1.PosY < 0 then
         p1.PosY = 0
-    elseif p1.PosY > love.graphics.getHeight() - p1.altura then
-        p1.PosY = love.graphics.getHeight() - p1.altura
+    elseif p1.PosY > love.graphics.getWidth() - p1.altura then
+        p1.PosY = love.graphics.getWidth() - p1.altura
     end
 
-    -- Colisoes
+    -- Arvores
 
-        -- Verificar colisão com as árvores
-        local colisao = false
-        for _, arvore in ipairs(arvores) do
-            if VerColisao(novoPosX, novoPosY, p1.largura, p1.altura, arvore.PosX, arvore.PosY, arvore.largura - 30, arvore.altura - 35) then
-                colisao = true
-                break  -- Não é necessário verificar mais colisões
-            end
+    if var1 then
+        for i = 1, 10 do
+            local novaArvore = {
+                largura = 80,
+                altura = 120,
+                PosX = math.random(0, love.graphics.getWidth() - 80),
+                PosY = math.random(0, love.graphics.getHeight() - 120)
+            }
+            table.insert(arvores, novaArvore)
         end
-    
-        -- Se não houver colisão, atualiza as posições
-        if not colisao then
-            p1.PosX = novoPosX
-            p1.PosY = novoPosY
-            arvores.var = false
-        end
+        var1 = false
+    end
 
+    -- Hitbox
+--...
 end
 
 function love.draw()
+    -- Background
     love.graphics.draw(background, 0, 0, 0, love.graphics.getWidth() / background:getWidth(), love.graphics.getHeight() / background:getHeight())
+    love.graphics.draw(Sprite1, p1.PosX,p1.PosY)
+    
+    -- Arvores II
 
--- Arvores
-
-    for i, arvore in ipairs(arvores) do
-        love.graphics.draw(tree, arvore.PosX, arvore.PosY)
+    for _, arvores in ipairs(arvores) do
+        love.graphics.draw(tree, arvores.PosX, arvores.PosY)
     end
-
--- Movimentacao
-
-    if p1.Direcao == "parado" or p1.Direcao == "cima" then
-        love.graphics.draw(Sprite1, p1.PosX, p1.PosY)
-    elseif p1.Direcao == "esquerda" or p1.Direcao == "direita" or p1.Direcao == "baixo" then
-        love.graphics.draw(Sprite2, p1.PosX, p1.PosY)
-    end
-    p1.Direcao = "parado"
 end
 
-function VerColisao(x1, y1, w1, h1, x2, y2, w2, h2)
-    return x1 < x2 + w2 and x1 + w1 > x2 and y1 < y2 + h2 and y1 + h1 > y2
-end
